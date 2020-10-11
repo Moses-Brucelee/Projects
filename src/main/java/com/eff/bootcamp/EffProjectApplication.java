a75c09763e622400ace327321ff7a89fe4ceb1ad
MosesBrucelee <Moses.Brucelee@gmail.com>
2020-10-11 16:31:34 +0200

    with starting files

---
+++ src/main/java/com/eff/bootcamp/EffProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eff.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffProjectApplication.class, args);
+	}
+
+}
